971935e402dc9fca5005a31e39d8a0ed8e61d9c9
unrSNOW <Yaroslav.sapon@bk.ru>
2023-11-27 13:20:26 +0300

    Добавьте файлы проекта.

---
+++ 7.2 высокий уровень зад 18/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    Console.WriteLine("введите предложение:");
+    string input = Console.ReadLine();
+    string[] words = input.Split(new[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+    int shortest = int.MaxValue;
+    foreach (string word in words)
+    {
+        if (word.Length < shortest)
+        {
+            shortest = word.Length;
+        }
+    }
+    if (shortest == int.MaxValue)
+    {
+        Console.WriteLine("в предложении нет слов");
+
+    }
+    else
+    {
+        Console.WriteLine($"Длина самого короткого слова:{shortest}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
